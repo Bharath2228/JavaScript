85d6e2e69b88b74d57c3b0e8eec8381102a80e53
Bharath2228 <pbharath2210@gmail.com>
2024-04-14 16:35:41 +0530

    Practiced some of the problems

--- Practice/Challenges/app.js
+++ Practice/Challenges/app.js
@@ -60,8 +60,67 @@
 //     console.log(arr[i])
 // }
 
-let sen = ["Hello,", "This", "is", "Bharath"]
+// let sen = ["Hello,", "This", "is", "Bharath"]
 
-for(let i = 0; i < sen.length; i++){
-    document.getElementById("greet-el").textContent += sen[i] + " "
+// for(let i = 0; i < sen.length; i++){
+//     document.getElementById("greet-el").textContent += sen[i] + " "
+// }
+
+let person = {
+    name: "Bharath",
+    age: 23,
+    country: "India"
+}
+
+function logData(){
+    let str = person.name + " is " + person.age + " years old and lives in " + person.country
+
+    console.log(str)
+}
+
+logData()
+
+let age = 66
+
+if (age < 6){
+    console.log("Free")
+} else if (age < 18){
+    console.log("Child Discount")
+} else if ( age < 27){
+    console.log("Student Discount")
+} else if (age < 67){
+    console.log("Full Price")
+}else {
+    console.log("Senior Citizen Discount")
+}
+
+let country = ["Tuvalu", "India", "USA", "Indonesia", "Monaco"]
+
+country.pop()
+country.push("Sri Lanka")
+country.shift()
+country.unshift("China")
+
+for(let i = 0; i<country.length; i++){
+    console.log("- " + country[i])
+}
+
+let hands = ['rock', 'paper', 'siscor']
+
+function game(){
+    let index = Math.floor(Math.random() * hands.length) 
+    console.log(hands.length)
+    return( hands[index] )
+}
+
+console.log(game())
+
+let num = [1, 2, 2, 1, 2, 1]
+
+for (let i = 0; i < num.length; i++){
+    if(num[i] === 1){
+        document.getElementById("one-shelf").textContent += num[i] + " " 
+    } else {
+        document.getElementById('two-shelf').textContent += num[i] + " "
+    }
 }
