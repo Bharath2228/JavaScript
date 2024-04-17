13a1fbd6ee7a158558979b177ccb44fc0a3d2671
Bharath2228 <pbharath2210@gmail.com>
2024-04-17 21:18:11 +0530

    learned different variabled

---
+++ Practice/Practice - Bro Code/app.js
@@ -0,0 +1,12 @@
+// console.log(`Hello`)
+
+// window.alert(`Hello this is a alert`)
+
+document.getElementById("myH1").textContent = 'Hello'
+document.getElementById("myP").textContent = `Paragraph`
+
+let x 
+x = 100
+console.log(typeof x)
+
+console.log(`Number ${x}`)
